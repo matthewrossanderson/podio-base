9cca614d636a3a8ce719821379c3fe8443d9eaa8
Matt Anderson <matthewrossanderson@gmail.com>
2013-03-13 10:30:38 -0400

    Comment out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
 # This seed file reads from two csvs - app names/ids, fieldnames/ids
 # and loads them into the database
 
-App.delete_all
-Field.delete_all
+# App.delete_all
+# Field.delete_all
 
-open(File.join(Rails.root, 'db', "app_seed.csv")).read.each_line do |app|
-  id, name = app.chomp.split(",")
-  a = App.create!(:id => id, :name => name)
-end
+# open(File.join(Rails.root, 'db', "app_seed.csv")).read.each_line do |app|
+#   id, name = app.chomp.split(",")
+#   a = App.create!(:id => id, :name => name)
+# end
 
-open(File.join(Rails.root, 'db', "field_seed.csv")).read.each_line do |field|
-  app_id, id, name = field.chomp.split(",")
-  next if app_id.nil?
-  f = Field.create!(:app_id=> app_id, :id => id, :name => name)
-end
+# open(File.join(Rails.root, 'db', "field_seed.csv")).read.each_line do |field|
+#   app_id, id, name = field.chomp.split(",")
+#   next if app_id.nil?
+#   f = Field.create!(:app_id=> app_id, :id => id, :name => name)
+# end
 
